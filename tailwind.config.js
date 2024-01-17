b16dd75d5ede55f90d886afecdd7d8acbdf9b5ce
yiannis <107484245+DimYiannis@users.noreply.github.com>
2024-01-17 20:16:05 +0200

    screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    screens: {
+      'mobile': '450px',
+        
+      'tablet': '760px',
+      // => @media (min-width: 760px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      'lgdesk' : '1600px',
+    },
+    extend: {
+      backgroundImage: {
+        'main' : "url('./assets/main.jpg')"
+      }
+    },
   },
   plugins: [],
 }
