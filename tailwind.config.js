f096b1e2afacdaa65a6b68ab0470161fa9cf856e
yiannis <107484245+DimYiannis@users.noreply.github.com>
2024-09-27 14:33:59 +0300

    design improvements

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,23 +2,16 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    screens: {
-      'mobile': '450px',
-        
-      'tablet': '760px',
-      // => @media (min-width: 760px) { ... }
-
-      'laptop': '1024px',
-      // => @media (min-width: 1024px) { ... }
-
-      'desktop': '1280px',
-      // => @media (min-width: 1280px) { ... }
-
-      'lgdesk' : '1600px',
-    },
     extend: {
+      screens: {
+        'mobile': '450px',
+        'tablet': '760px',
+        'laptop': '1024px',
+        'desktop': '1280px',
+        'lgdesk': '1600px',
+      },
       backgroundImage: {
-        'main' : "url('./assets/main.jpg')"
+        'main': "url('./assets/main.jpg')"
       }
     },
   },
